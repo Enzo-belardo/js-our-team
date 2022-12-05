d3e311ae9c3e3bb6bdcc0628f5a85033d2656c64
Enzo-belardo <vincenzobelardo10@gmail.com>
2022-12-05 16:12:01 +0100

    creato array di oggetti

---
+++ script.js
@@ -0,0 +1,39 @@
+
+const team =[
+   {
+    nome : 'Wayne Barnett',
+    ruolo : 'Founder & CEO' ,
+    foto : 'wayne-barnett-founder-ceo.jpg',
+   },
+
+   {
+    nome : 'Angela caroll',
+    ruolo : 'Chief Editor' ,
+    foto : 'angela-caroll-chief-editor.jpg',
+   },
+
+   {
+    nome : 'Walter Gordon',
+    ruolo : 'Office Menager' ,
+    foto : 'walter-gordon-office-manager.jpg',
+   },
+
+   {
+    nome : 'Angela lopez',
+    ruolo : 'Social media' ,
+    foto : 'Manager  angela-lopez-social-media-manager.jpg',
+   },
+
+   {
+    nome : 'Scott Estrada',
+    ruolo : 'Developer' ,
+    foto : 'scott-estrada-developer.jpg',
+   },
+
+   {
+    nome : 'Barbara Ramos',
+    ruolo : 'Graphic designer' ,
+    foto : 'barbara-ramos-graphic-designer.jpg',
+   },
+]
+
